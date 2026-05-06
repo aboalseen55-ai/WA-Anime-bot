2aa21a83b9a9a32615454670c08e6605145ff95d
Your Name <your-email@example.com>
2026-05-07 01:04:03 +0300

    Remove secrets from config file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,11 +6,9 @@ module.exports = {
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
+    env_file: '.env',
     env: {
-      NODE_ENV: 'production',
-      MONGO_URI: 'mongodb+srv://aboalseen55_db_user:YjIGM09wB2lGRbdB@samc01.vhez6tz.mongodb.net/?appName=SamC01',
-      GOOGLE_API_KEY: 'AIzaSyAMQ5yASneJiKcwMKSDAkjyu-ilQry4PPQ',
-      OPENAI_API_KEY: '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'
+      NODE_ENV: 'production'
     },
     error_file: './logs/err.log',
     out_file: './logs/out.log',
